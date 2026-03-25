45f9a0d0190401e98ab5f2ac6adf97046caeeca9
dev-yeorin <par4mit4@gmail.com>
2026-03-25 15:05:46 +0900

    상속

---
+++ src/ex03/TestAnimal.java
@@ -0,0 +1,40 @@
+package ex03;
+
+class Dog {
+	String name;
+	void 	eat() {
+		System.out.println(name + "(이)가 먹는다");
+	}
+	void	bark() {
+		System.out.println(name + "(이)가 멍멍");
+	}
+}
+
+class Cat {
+	String name;
+	void 	eat() {
+		System.out.println(name + "(이)가 먹는다");
+	}
+	void	meow() {
+		System.out.println(name + "(이)가 야옹");
+	}
+	
+}
+
+
+public class TestAnimal {
+
+	public static void main(String[] args) {
+		Dog chu = new Dog(); // 생성자 호출 -> 생성자 없음 : 자바가 생성해 줌
+		chu.name = "츄";
+		chu.eat();
+		chu.bark();
+		
+		Cat nero = new Cat();
+		nero.name = "네로";
+		nero.eat();
+		nero.meow();
+
+	}
+
+}
